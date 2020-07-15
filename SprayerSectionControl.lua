a4783919ec313b84969b3b3768e693e07da610ec
Lukas Fabian <32129976+LukasFabian@users.noreply.github.com>
2020-07-15 20:28:23 +0200

    v0.1.2.0
    #3

--- SprayerSectionControl.lua
+++ SprayerSectionControl.lua
@@ -1,11 +1,13 @@
 --
 -- SprayerSectionControl
 --
--- # Author:  Rival
--- # date: 24.06.2020
+-- # author:	Rival
+-- # date:		15.07.2020
+-- # version:	0.1.2.0
 
 
 SprayerSectionControl = {}
+SprayerSectionControl.modDirectory  = g_currentModDirectory
 
 function SprayerSectionControl.prerequisitesPresent(specializations)
     return true
@@ -15,6 +17,7 @@ function SprayerSectionControl.registerEventListeners(vehicleType)
 	SpecializationUtil.registerEventListener(vehicleType, "onLoad", SprayerSectionControl)
 	SpecializationUtil.registerEventListener(vehicleType, "onUpdate", SprayerSectionControl)
 	SpecializationUtil.registerEventListener(vehicleType, "onTurnedOn", SprayerSectionControl)
+	SpecializationUtil.registerEventListener(vehicleType, "onAIImplementStart", SprayerSectionControl)
 	--SpecializationUtil.registerEventListener(vehicleType, "onDraw", SprayerSectionControl)
 end
 
@@ -26,6 +29,7 @@ end
 function SprayerSectionControl.registerFunctions(vehicleType)
 	SpecializationUtil.registerFunction(vehicleType, "getSprayerFullWidth", SprayerSectionControl.getSprayerFullWidth)
 	SpecializationUtil.registerFunction(vehicleType, "getActiveSprayerSectionsWidth", SprayerSectionControl.getActiveSprayerSectionsWidth)
+	SpecializationUtil.registerFunction(vehicleType, "changeSectionState", SprayerSectionControl.changeSectionState)
 end
 
 function SprayerSectionControl:onLoad(savegame)
@@ -45,24 +49,24 @@ function SprayerSectionControl:onLoad(savegame)
 			end
 			i = i + 1
 			local workAreaId = getXMLInt(self.xmlFile, key.."#workAreaId")
+			--local sprayType = getXMLInt(self.xmlFile, key.."#sprayType")
 			local effectNodes = StringUtil.splitString(" ", StringUtil.trim(getXMLString(self.xmlFile, key.."#effectNodeId")))
 			local testAreaStart = I3DUtil.indexToObject(self.components, getXMLString(self.xmlFile, key .. "#testAreaStartNode"), self.i3dMappings)
 			local testAreaWidth = I3DUtil.indexToObject(self.components, getXMLString(self.xmlFile, key .. "#testAreaWidthNode"), self.i3dMappings)
 			local testAreaHeight = I3DUtil.indexToObject(self.components, getXMLString(self.xmlFile, key .. "#testAreaHeightNode"), self.i3dMappings)
-			local workingWidth = Utils.getNoNil(getXMLFloat(self.xmlFile, key.."#workingWidth"), 2)
+			local workingWidth = Utils.getNoNil(getXMLFloat(self.xmlFile, key.."#workingWidth"), 3)
 			if workAreaId == nil and self.spec_workArea.workAreas[i] ~= nil then
 				workAreaId = i
 			end
-			--if effectNodes == nil and self.spec_sprayer.effects[i] ~= nil then
-			--	effectNodes = i
-			--end
 			if workAreaId ~= nil and effectNodes ~= nil and testAreaStart ~= nil and testAreaWidth ~= nil and testAreaHeight ~= nil then
 				spec.sections[i] = {workAreaId=workAreaId, effectNodes=effectNodes, testAreaStart=testAreaStart, testAreaWidth=testAreaWidth, testAreaHeight=testAreaHeight, active=true, workingWidth = workingWidth}
 				self.spec_workArea.workAreas[workAreaId].sscId = i
+				spec.sections[i].sprayType = self.spec_workArea.workAreas[workAreaId].sprayType
 			else
 				print("Warning: Invalid sprayer section setup '"..key.."' in '" .. self.configFileName.."'")
 			end
 		end
+		spec.isAutomaticMode = true
 	end
 	
 	self.spec_ssc = spec
@@ -71,7 +75,7 @@ end
 function SprayerSectionControl:onUpdate(dt)
 	if self.spec_ssc.isSSCReady then
 		local spec = self.spec_ssc
-		if self:getIsTurnedOn() and not self:getIsAIActive() then
+		if spec.isAutomaticMode and self:getIsTurnedOn() and not self:getIsAIActive() then
 			if #spec.sections > 0 then
 				local fillType = self:getFillUnitLastValidFillType(self:getSprayerFillUnitIndex())
 				if fillType == FillType.UNKNOWN then
@@ -94,31 +98,21 @@ function SprayerSectionControl:onUpdate(dt)
 					end
 				end
 				for k,section in pairs(spec.sections) do
-					local sx,_,sz = getWorldTranslation(section.testAreaStart)
-					local wx,_,wz = getWorldTranslation(section.testAreaWidth)
-					local hx,_,hz = getWorldTranslation(section.testAreaHeight)
-					local area, totalArea = AIVehicleUtil.getAIFruitArea(sx, sz, wx, wz, hx, hz, self:getFieldCropsQuery())
-					local newState = area > 0
-					if section.active ~= newState then
-						section.active = newState
-						--print(string.format("Section %d was turned %s", section.workAreaId, newState and "on" or "off"))
-						if newState then
-							if self:getAreEffectsVisible() then
-								for k2,effectNodeId in pairs(section.effectNodes) do
-									g_effectManager:startEffect(self.spec_sprayer.effects[tonumber(effectNodeId)])
-									local sprayType = self:getActiveSprayType()
-									if sprayType ~= nil then
-										g_effectManager:startEffect(sprayType.effects[tonumber(effectNodeId)])
-									end
-								end
-							end
-						else
-							for k2,effectNodeId in pairs(section.effectNodes) do
-								g_effectManager:stopEffect(self.spec_sprayer.effects[tonumber(effectNodeId)])
-								for _, sprayType in ipairs(self.spec_sprayer.sprayTypes) do
-									g_effectManager:stopEffect(sprayType.effects[tonumber(effectNodeId)])
-								end
-							end
+					local sActive = true
+					if section.sprayType ~= nil then
+						local sprayType = self:getActiveSprayType()
+						if sprayType ~= nil and section.sprayType ~= sprayType.index then
+							sActive = false
+						end
+					end
+					if sActive then
+						local sx,_,sz = getWorldTranslation(section.testAreaStart)
+						local wx,_,wz = getWorldTranslation(section.testAreaWidth)
+						local hx,_,hz = getWorldTranslation(section.testAreaHeight)
+						local area, totalArea = AIVehicleUtil.getAIFruitArea(sx, sz, wx, wz, hx, hz, self:getFieldCropsQuery())
+						local newState = area > 0
+						if section.active ~= newState then
+							self:changeSectionState(section, newState)
 						end
 					end
 				end
@@ -127,8 +121,34 @@ function SprayerSectionControl:onUpdate(dt)
 	end
 end
 
---function SprayerSectionControl:onDraw()
---end
+function SprayerSectionControl:changeSectionState(section, newState)
+	if newState == nil then
+		newState = not section.active
+	end
+	section.active = newState
+	if newState and self:getIsTurnedOn() then
+		if self:getAreEffectsVisible() then
+			for k2,effectNodeId in pairs(section.effectNodes) do
+				local sprayType = self:getActiveSprayType()
+				if sprayType ~= nil then
+					g_effectManager:startEffect(sprayType.effects[tonumber(effectNodeId)])
+				else
+					g_effectManager:startEffect(self.spec_sprayer.effects[tonumber(effectNodeId)])
+				end
+			end
+		end
+	else
+		for k2,effectNodeId in pairs(section.effectNodes) do
+			g_effectManager:stopEffect(self.spec_sprayer.effects[tonumber(effectNodeId)])
+			local sprayType = self:getActiveSprayType()
+			if sprayType ~= nil then
+				g_effectManager:stopEffect(sprayType.effects[tonumber(effectNodeId)])
+			else
+				g_effectManager:stopEffect(self.spec_sprayer.effects[tonumber(effectNodeId)])
+			end
+		end
+	end
+end
 
 function SprayerSectionControl:onTurnedOn()
 	if self.spec_ssc.isSSCReady then
@@ -145,6 +165,14 @@ function SprayerSectionControl:onTurnedOn()
 	end
 end
 
+function SprayerSectionControl:onAIImplementStart() -- turn on every section at AI hire
+	if self.spec_ssc.isSSCReady then
+		for k,section in pairs(self.spec_ssc.sections) do
+			self:changeSectionState(section, true)
+		end
+	end
+end
+
 function SprayerSectionControl:getSprayerUsage(superFunc, fillType, dt)
 	local origUsage = superFunc(self, fillType, dt)
 	if self.spec_ssc.isSSCReady then
@@ -156,7 +184,7 @@ end
 
 function SprayerSectionControl:processSprayerArea(superFunc, workArea, dt)
 	if self.spec_ssc.isSSCReady then
-		if not self.spec_ssc.sections[workArea.sscId].active then
+		if workArea.sscId ~= nil and not self.spec_ssc.sections[workArea.sscId].active then
 			return 0,0
 		end
 	end
